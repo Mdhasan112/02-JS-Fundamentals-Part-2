6b92d537de001a948aba59e7e2baf8511f396ec4
hasan <mdhasanhk.98@gmail.com>
2022-11-06 23:39:55 +0600

    first commit

---
+++ starter/script.js
@@ -0,0 +1,54 @@
+'use strict';
+// let driverLicense = false;
+// const pass = true;
+
+// if (pass) driverLicense = true;
+// if (driverLicense) console.log('i can drive')
+
+/*
+function logger() {
+    console.log('Hello World!')
+};
+
+logger();
+logger();
+
+function foodProcessor(apple, orange) {
+    console.log(apple + orange);
+    const juice = `i have ${apple} apple and ${orange} orange juices.`
+    return juice;
+};
+
+const appleJuice = foodProcessor(10, 5);
+console.log(appleJuice);
+*/
+
+/*
+//function declaration
+function calAge(birthYear) {
+    const age = 2050 - birthYear;
+    return age;
+};
+const age1 = calAge(2010);
+
+//function expression
+const calAge2 = function (birthYear) {
+    return 2050 - birthYear;
+};
+const age2 = calAge2(2010);
+
+console.log(age1, age2);
+
+//Arrow function
+const calAge3 = birthYear => 2050 - birthYear;
+const age3 = calAge3(2010);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2050 - birthYear;
+    const retirement = 70 - age;
+    return `${firstName} retires in ${retirement} years`
+}
+console.log(yearsUntilRetirement(1998, 'hasan'));
+console.log(yearsUntilRetirement(2005, 'rob')); 
+*/
