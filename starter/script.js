563296a98ef6479b9072612abd32047c8fad9e48
hasan <mdhasanhk.98@gmail.com>
2022-12-02 12:23:19 +0600

    for loop

--- starter/script.js
+++ starter/script.js
@@ -1,10 +1,15 @@
-'use strict';
+"use strict";
+
+///////////////////////////////////////
+// Activating Strict Mode
 // let driverLicense = false;
 // const pass = true;
 
 // if (pass) driverLicense = true;
 // if (driverLicense) console.log('i can drive')
 
+/////////////////////////////////////
+//functions
 /*
 function logger() {
     console.log('Hello World!')
@@ -23,7 +28,7 @@ const appleJuice = foodProcessor(10, 5);
 console.log(appleJuice);
 */
 
-/*
+/*///////////////////////////////
 //function declaration
 function calAge(birthYear) {
     const age = 2050 - birthYear;
@@ -31,6 +36,7 @@ function calAge(birthYear) {
 };
 const age1 = calAge(2010);
 
+///////////////////////////////////////
 //function expression
 const calAge2 = function (birthYear) {
     return 2050 - birthYear;
@@ -39,6 +45,7 @@ const age2 = calAge2(2010);
 
 console.log(age1, age2);
 
+//////////////////////////////////////
 //Arrow function
 const calAge3 = birthYear => 2050 - birthYear;
 const age3 = calAge3(2010);
@@ -53,7 +60,7 @@ console.log(yearsUntilRetirement(1998, 'hasan'));
 console.log(yearsUntilRetirement(2005, 'rob'));
 */
 
-/*
+/*////////////////////////////////////////
 //function into a function
 function cutFood(fruits) {
     return fruits * 4;
@@ -70,6 +77,8 @@ const result = foodProcessor(2, 3);
 console.log(result)
 */
 
+//////////////////////////////
+//Review functions
 /*
 const calcAge = function (birthYear) {
     return 2050 - birthYear;
@@ -91,6 +100,25 @@ console.log(yearsUntilRetirement(2000, 'John'));
 console.log(yearsUntilRetirement(1971, 'John'))
 */
 
+//////////////////////////////////////
+// Coding Challenge #1
+/*
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+Each team competes 3 times and then the average of the 3 scores ,is calculated (so one average score per team).
+A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
+
+1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+2. Use the function to calculate the average for both teams
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the consothe winner to le, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
+5. Ignore draws this time.
+
+TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+TEST DATA 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+
+HINT: To calculate average of 3 values, add them all together and divide by 3
+HINT: To check if number A is at least double number B, check for A >= 2 * B. Apply this to the team's average scores 
+*/
 
 /*
 const calcAverage = (a, b, c) => (a + b + c) / 3;
@@ -113,6 +141,8 @@ const checkWinner = function (avgDolphins, avgKoalas) {
 checkWinner(scoreDolphins, scoreKoalas);
 */
 
+////////////////////////////////
+// Introduction to Arrays
 /*
 const friend1 = 'Michael';
 const friend2 = 'Ross Tailor';
@@ -168,11 +198,11 @@ if (friends.includes('Ross Tailor')) {
 
 //coding challenge- 2
 const calcTip = function (bill) {
-    return
-}
+  return;
+};
 
-/*
-//object
+/*////////////////////////////////////////
+//object 
 const jonasArray = [
     'Jonas',
     'william',
@@ -226,8 +256,20 @@ console.log(jonas.age);
 // console.log(jonas['calcAge'](2000));\
 */
 
+/////////////////////////////////////
+// Coding Challenge #3
+
 /*
-//challenge- 3
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method.
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+*/
+
+/*
 const MarkMillerBio = {
     fullName: 'Mark Miller',
     mass: 78,
@@ -259,6 +301,7 @@ if (MarkMillerBio.calcBmi() > johnSmithBio.BMI) {
 }
 */
 
+/////////////////////////////////////////
 /*
 //forloop
 console.log('Lifting weights repeating 1');
@@ -300,6 +343,7 @@ for (let i = 0; i < jonas.length; i++) {
 };
 */
 
+//////////////////////////////////
 /*
 //loop in loop
 
@@ -347,16 +391,16 @@ if (dice !== 6) {
 
 // test challenge
 const calcTips = function (bills) {
-    return bills >= 50 && bills <= 300 ? bills * 0.15 : bills * 0.2;
-}
+  return bills >= 50 && bills <= 300 ? bills * 0.15 : bills * 0.2;
+};
 
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 const tips = [];
 const total = [];
 
 for (let i = 0; i < bills.length; i++) {
-    const tip = calcTips((bills[i]));
-    tips.push(tip);
-    total.push(tip + (bills[i]))
+  const tip = calcTips(bills[i]);
+  tips.push(tip);
+  total.push(tip + bills[i]);
 }
-console.log(tips, total)
+console.log(tips, total);
